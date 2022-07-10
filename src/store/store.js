dc6efc532c56b1ecc88dd06919db353c73ed9ca4
Laurence Trippen <laurence.trippen@smartelephantapps.com>
2022-07-10 23:53:52 +0400

    removed logs

--- src/store/store.js
+++ src/store/store.js
@@ -22,7 +22,6 @@ export const useStore = create((set) => ({
   updateMarker: (updatedPosition) => set((state) => {
     return ({
       markerPositions: state.markerPositions.map((pos) => {
-  
         if (pos.uuid !== updatedPosition.uuid) {
           // This isn't the item we care about - keep it as-is
           return pos;
@@ -33,8 +32,6 @@ export const useStore = create((set) => ({
           ...pos,
           ...updatedPosition,
         };
-  
-        console.log(updated);
   
         return updated;
       })
