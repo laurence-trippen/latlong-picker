9e5f7eedf06cb1c10383e99161d988bd44c7ff3b
Laurence Trippen <laurence.trippen@smartelephantapps.com>
2022-07-10 21:30:41 +0400

    drawer (wip)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,16 +7,17 @@ import { blueGrey } from '@mui/material/colors';
 import { ADD_MODE, DRAG_MODE } from '../core/modes';
 import { useStore } from '../store/store';
 
-function Header({ onMyLocation }) {
+function Header({ onMyLocation, onMenu, open }) {
   const selectedMode = useStore((state) => state.selectedMode);
   const setMode = useStore((state) => state.setMode);
 
   const handleOnChangeMode = (e, newMode) => setMode(newMode);
 
   return (
-    <AppBar position="relative" style={{ background: "white", color: blueGrey[600], zIndex: 10000 }}>
+    <AppBar position="relative" open={open} style={{ background: "white", color: blueGrey[600], zIndex: 10000 }}>
       <Toolbar>
         <IconButton
+          onClick={onMenu}
           size="large"
           edge="start"
           color="inherit"
--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,14 +1,29 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import { Box } from '@mui/system';
 import MapLocation from '../components/MapLocation';
 import Header from '../components/Header';
 import { useStore } from '../store/store';
 import MapEvents from '../components/MapEvents';
-import { IconButton } from '@mui/material';
+import { Divider, Drawer, IconButton, ListItem, ListItemIcon, ListItemText, useTheme } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+const drawerWidth = 240;
 
 function MapPage() {
+  // State
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [mockPos, setMockPos] = useState([
+    { lat: 5, long: 2 },
+    { lat: 5, long: 2 },
+    { lat: 5, long: 2 },
+  ]);
+
+  // Hooks
+  const theme = useTheme();
+  console.log('Theme: ', theme);
+
   // Store
   const markerPositions = useStore((state) => state.markerPositions);
   const removeMarker = useStore((state) => state.removeMarker);
@@ -21,41 +36,86 @@ function MapPage() {
     mapLocationRef.current.flyToMyLocation();
   }
 
+  function handleToggleDrawer() {
+    setDrawerOpen(state => !state);
+  }
+
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", width: "100%", height: "100vh" }}>
-      <Header onMyLocation={handleMyLocation} />
-      <Box sx={{ flexGrow: 1 }}>
-        <MapContainer 
-          style={{ height: "100%" }} 
-          center={[51.505, -0.09]} 
-          zoom={13} 
-          scrollWheelZoom={true}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
+    <>
+      <Box sx={{ display: "flex", flexDirection: "column", width: "100%", height: "100vh" }}>
+        <Header 
+          onMyLocation={handleMyLocation} 
+          onMenu={handleToggleDrawer}
+          open={drawerOpen} 
+        />
+        <Box sx={{ flexGrow: 1 }}>
+          <Box sx={{ display: "flex", height: "100%" }}>
+            <MapContainer 
+              style={{
+                height: "100%", 
+                flexGrow: 1,
+              }}
+              className={drawerOpen}
+              center={[51.505, -0.09]} 
+              zoom={13} 
+              scrollWheelZoom={true}
+            >
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={[51.505, -0.09]}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
 
-          {markerPositions.map((position, id) => (
-            <Marker key={`marker-${id}`} position={position} draggable={true}>
-              <Popup>
-                <IconButton size='small' onClick={() => removeMarker(position)}>
-                  <DeleteIcon />
-                </IconButton>
-              </Popup>
-            </Marker>
-          ))}
+              {markerPositions.map((position, id) => (
+                <Marker key={`marker-${id}`} position={position} draggable={true}>
+                  <Popup>
+                    <IconButton size='small' onClick={() => removeMarker(position)}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </Popup>
+                </Marker>
+              ))}
 
-          <MapLocation ref={mapLocationRef} zoom={10} />
-          <MapEvents />
-        </MapContainer>
+              <MapLocation ref={mapLocationRef} zoom={10} />
+              <MapEvents />
+            </MapContainer>
+            <Drawer
+              variant="permanent"
+              anchor="right"
+              open={drawerOpen}
+              sx={{
+                width: drawerWidth,
+                position: "relative",
+                overflow: "hidden",
+                marginLeft: "auto",
+                flexShrink: 0,
+                '& .MuiDrawer-paper': {
+                  width: drawerWidth,
+                  position: "absolute",
+                  boxSizing: 'border-box',
+                },
+              }}
+            >
+              {mockPos.map((pos, id) => (
+                <>
+                  <ListItem key={id}>
+                    <ListItemIcon>
+                      <LocationOnIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={`${pos.lat} ${pos.long}`} />
+                  </ListItem>
+                  <Divider />
+                </>
+              ))}
+            </Drawer>
+          </Box>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 }
 
