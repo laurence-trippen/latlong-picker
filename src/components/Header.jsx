a6fe3250ab0c57d9569987b9b279f692547056d1
Laurence Trippen <laurence.trippen@smartelephantapps.com>
2022-07-10 11:25:25 +0400

    added toolbar mode ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
-import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material';
+import PanToolIcon from '@mui/icons-material/PanTool';
+import AddLocationAltIcon from '@mui/icons-material/AddLocationAlt';
+import { AppBar, Button, IconButton, ToggleButton, ToggleButtonGroup, Toolbar, Typography } from '@mui/material';
 import { blueGrey } from '@mui/material/colors';
 
+const DRAG_MODE = "drag";
+const ADD_MODE = "add";
+
 function Header({ onMyLocation }) {
+  const [mode, setMode] = useState(DRAG_MODE);
+
+  const handleOnChangeMode = (e, newMode) => setMode(newMode);
+
   return (
     <AppBar position="relative" style={{ background: "white", color: blueGrey[600], zIndex: 10000 }}>
       <Toolbar>
@@ -16,10 +25,27 @@ function Header({ onMyLocation }) {
         >
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Typography variant="h6" component="div">
           LatLong Picker
         </Typography>
-        <Button onClick={onMyLocation}>My Location</Button>
+        <ToggleButtonGroup 
+          value={mode} 
+          size="small" 
+          exclusive 
+          onChange={handleOnChangeMode}
+          sx={{ marginLeft: "3rem" }}
+        >
+          <ToggleButton value={DRAG_MODE}>
+            <PanToolIcon />
+          </ToggleButton>
+          <ToggleButton value={ADD_MODE}>
+            <AddLocationAltIcon />
+          </ToggleButton>
+        </ToggleButtonGroup>
+        
+        <div style={{ marginLeft: "auto" }}>
+          <Button onClick={onMyLocation}>My Location</Button>
+        </div>
       </Toolbar>
     </AppBar>
   )
